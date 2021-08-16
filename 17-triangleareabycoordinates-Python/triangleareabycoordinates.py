6348355da794df6f56d3914ccfdccd2752cb6905
Pragya <pragyajha2812@msitprogram.net>
2021-08-16 09:09:47 +0530

    solved

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -4,6 +4,18 @@
 # area of the triangle formed by those three points. Hint: you should make constructive use of
 # the triangleArea function you just wrote above.
 
+import math as m
+
+def distance(x1, y1, x2, y2):
+	return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
+
+def trianglearea(s1, s2, s3):
+	s = (s1 + s2 + s3)/2
+	a = m.sqrt(s * (s-s1) * (s-s2) * (s-s3))
+	return a
+
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
-	# your code goes here
-	pass
+	s1 = distance(x1, y1, x2, y2)
+	s2 = distance(x2, y2, x3, y3)
+	s3 = distance(x3, y3, x1, y1)
+	return trianglearea(s1, s2, s3)
