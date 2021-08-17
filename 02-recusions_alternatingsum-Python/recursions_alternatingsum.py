fd504ebb78e1c91422f6235d84c1eaf1bfcd1375
Pragya <pragyajha2812@msitprogram.net>
2021-08-17 09:01:12 +0530

    solved

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -3,6 +3,12 @@
 # rather than added. For example: alternatingSum([1,2,3,4,5]) returns 1-2+3-4+5 
 # (that is, 3). If L is empty, return 0. You may not use loops/iteration in this problem.
 
+def alternatingSum(L, s = 0, sign = 1):
+	if len(L) == 0:
+		return s
+	s = s + sign*L[0]
+	sign = -sign
+	return alternatingSum(L[1:], s, sign)
 
 def fun_recursions_alternatingsum(l): 
-	return 0
+	return alternatingSum(l)
