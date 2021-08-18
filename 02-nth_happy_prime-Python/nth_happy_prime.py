aad5ec8aa477829d80044b9d8f92953d4de5717d
Pragya <pragyajha2812@msitprogram.net>
2021-08-18 10:00:01 +0530

    solved

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -2,7 +2,36 @@
 # Write the function nthHappyPrime(n) which takes a non-negative integer 
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
 
+def isPrime(n):
+	if n < 2:
+		return False
+	if n == 2:
+		return True
+	if n % 2 == 0:
+		return False
+	for i in range(3, int(n ** 0.5) + 1, 2):
+		if n % i == 0:
+			return False
+	return True
 
+def sumOfSq(n):
+	s = 0
+	while n > 0:
+		s += (n % 10) ** 2
+		n //= 10
+	return s
 
 def fun_nth_happy_prime(n):
-	return 0
+	k = 0
+	i = 0
+	while (k <= n):
+		i += 1
+		if isPrime(i):
+			num = sumOfSq(i)
+			while num > 9:
+				num = sumOfSq(num)
+			if num == 1:
+				k += 1
+	return i
+
+print(fun_nth_happy_prime(1))
