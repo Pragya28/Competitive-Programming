b9d2283c4089fb2fb9e9d674ac3a9083e86ccde2
Pragya <pragyajha2812@msitprogram.net>
2021-08-19 11:53:22 +0530

    solved

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -2,9 +2,13 @@
 # frequent character in the string. If there are multiple characters that 
 # can account as K-th the most frequent character then, print any one of them.
 
-
+from collections import defaultdict
 
 def fun_kth_occurrences(s, n):
-	return 'a'
+	freq = defaultdict(int)
+	for ch in s:
+		freq[ch] += 1
+	d = sorted(freq.items(), key = lambda x: (x[1], x[0]), reverse = True)
+	return d[n-1][0]
 
 
