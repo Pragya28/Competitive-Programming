3962c60cdf4a34be080a600aed885849f6b53765
Pragya <pragyajha2812@msitprogram.net>
2021-08-21 21:00:55 +0530

    solved

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -9,6 +9,22 @@
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
+def count(L, start):
+	c = 1
+	while start < len(L)-1:
+		if L[start] == L[start+1]:
+			c += 1
+			start += 1
+		else:
+			break
+	return c
+
+
 def lookandsay(a):
-	# Your code goes here
-	pass
+	i = 0
+	res = []
+	while i < len(a):
+		n = count(a, i)
+		res.append((n, a[i]))
+		i += n
+	return res
