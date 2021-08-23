1f77cd25bdd272f627f085df8dc207c95c688335
Pragya <pragyajha2812@msitprogram.net>
2021-08-23 09:42:08 +0530

    solved

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -5,6 +5,42 @@
 # L, return a new list M such that M is the same as L, only with exactly one value changed, and M is a mostly magic 
 # square.
 
+def check(a):
+	for i in range(1, len(a)):
+		if sum(a[i]) != sum(a[i-1]):
+			return (i-1, i)
+	return -1
 
 def fixmostlymagicsquare(L):
-	# Your code goes here
+	rows = check(L)
+	c = []
+	for i in range(len(L[0])):
+		c.append([L[j][i] for j in range(len(L))])
+	cols = check(c)
+	S = 0
+	for i in range(len(L)):
+		if i not in rows:
+			for j in rows:
+				if sum(L[i]) != sum(L[j]):
+					rows = j
+					S = sum(L[i])
+					break
+			if isinstance(rows, int):
+				break
+	for i in range(len(c)):
+		if i not in cols:
+			for j in cols:
+				if sum(c[i]) != sum(c[j]):
+					cols = j
+					break
+			if isinstance(cols, int):
+				break
+	M = [[0] * len(L) for _ in range(len(L))]
+	for i in range(len(M)):
+		for j in range(len(M)):
+			if i == rows and j == cols:
+				M[i][j] = L[i][j] + (S - sum(L[i]))
+			else:
+				M[i][j] = L[i][j]
+	return M
+print(fixmostlymagicsquare([[2, 7, 9], [9, 5, 1], [4, 3, 8]]))
