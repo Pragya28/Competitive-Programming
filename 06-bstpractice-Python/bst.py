4f339ff8ad97e6432136baf0cb92e2e4051ae4f5
Pragya <pragyajha2812@msitprogram.net>
2021-08-23 09:14:17 +0530

    solved - printSelf() not done

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -9,14 +9,38 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        # Your code goes here
-        pass
+        if self.root:
+            x = self.root
+            while True:
+                if x.value > new_val:
+                    if x.left:
+                        x = x.left
+                    else:
+                        x.left = Node(new_val)
+                        break
+                elif x.value < new_val:
+                    if x.right:
+                        x = x.right
+                    else:
+                        x.right = Node(new_val)
+                        break
+        else:
+            self.root = new_val
+
 
     def printSelf(self):
         # Your code goes here
         pass
         
     def search(self, find_val):
-        # Your code goes here
-        pass
-
+        if not isinstance(find_val, int):
+            return False
+        x = self.root
+        while x != None:
+            if x.value == find_val:
+                return True
+            elif x.value > find_val:
+                x = x.left
+            elif x.value < find_val:
+                x = x.right
+        return False
