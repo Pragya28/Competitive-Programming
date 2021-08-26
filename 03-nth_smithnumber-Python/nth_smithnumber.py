6fa2663a66191b0cf39224633da773d3cbfd2417
Pragya <pragyajha2812@msitprogram.net>
2021-08-26 16:04:19 +0530

    solved

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,46 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def sumDigits(n):
+    s = 0
+    while n > 0:
+        s += n % 10
+        n //= 10
+    return s
+
+def isPrime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 def fun_nth_smithnumber(n):
-    return 1
+    i = 0
+    x = 3
+    while i <= n:
+        x += 1
+        if not isPrime(x):
+            k = x
+            pf = []
+            f = 2
+            while f <= k:
+                if k % f == 0:
+                    if isPrime(f):
+                        pf.append(f)
+                    k //= f
+                else:
+                    f += 1
+            s = 0
+            for f in pf:
+                s += sumDigits(f)
+            if s == sumDigits(x):
+                i += 1
+    return x
+
+print(fun_nth_smithnumber(1))
