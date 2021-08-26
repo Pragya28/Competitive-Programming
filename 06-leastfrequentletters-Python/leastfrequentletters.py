082acace3094ae0b9e058cafc3faa5bad697009d
Pragya <pragyajha2812@msitprogram.net>
2021-08-26 10:57:14 +0530

    solved

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -7,6 +7,31 @@
 # yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
+def leastfrequency(s):
+	f = len(s)
+	for ch in s:
+		c = s.count(ch)
+		f = min(f, c)
+	return f
+
 def leastfrequentletters(s):
-	# Your code goes here
-	pass
+	s = s.lower()
+	alphs = ""
+	for ch in s:
+		if ch.isalpha():
+			alphs += ch
+	if alphs == "":
+		return ""
+	freq = leastfrequency(alphs)
+	s = ""
+	for ch in alphs:
+		if alphs.count(ch) == freq:
+			s += ch
+	res = ""
+	for i in range(26):
+		if len(res) == len(s):
+			return res
+		ch = chr(ord("a")+i)
+		if ch in s:
+			res += ch
+	return res
