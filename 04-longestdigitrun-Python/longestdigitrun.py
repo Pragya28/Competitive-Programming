1e90559094cea500f692b129c6e6ef56a2634bb0
Pragya <pragyajha2812@msitprogram.net>
2021-08-27 09:21:45 +0530

    solved

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -4,6 +4,29 @@
 # run, or the smallest such digit if there is a tie. So, longestDigitRun(117773732) returns 7 (
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
+
+def run(n):
+	c = 1
+	while n > 0:
+		a = n % 10
+		n //= 10
+		b = n % 10
+		if a == b:
+			c += 1
+		else:
+			break
+	return c, a
+
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+	n = abs(n)
+	cnt = 0
+	k = n % 10
+	while n > 0:
+		(c, x) = run(n)
+		if cnt == c:
+			k = min(x, k)
+		if cnt < c:
+			cnt = c
+			k = x
+		n = n // 10 ** c
+	return k
