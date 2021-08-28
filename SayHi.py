f2140d94db9bb79ca6902c865bdff3a197010d88
Pragya <pragyajha2812@msitprogram.net>
2021-08-28 11:44:13 +0530

    solved

--- SayHi.py
+++ SayHi.py
@@ -4,5 +4,24 @@
 # sayHi(d1) # Hi, my name is Dot and I am 4 years old!
 # sayHi(d2) # Hi, my name is Elf and I am 3 years old!
 
-class SayHi(Object):
-    pass
+class Dog():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        s = "my name is " + self.name + " and I am " + str(self.age) +  " years old!"
+        return s
+
+class SayHi(object):
+    def __init__(self, obj):
+        self.obj = obj        
+    
+    def __str__(self):
+        return "Hi, " + self.obj.__str__()
+
+d1 = Dog("Dot", 4)
+d2 = Dog("Elf", 3)
+assert(str(SayHi(d1)) == "Hi, my name is Dot and I am 4 years old!")
+assert(str(SayHi(d2)) == "Hi, my name is Elf and I am 3 years old!")
+print("Test cases passed")
