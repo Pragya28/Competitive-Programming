cfdb183183165abc6137962ffd2fcb879bc6619d
Pragya <pragyajha2812@msitprogram.net>
2021-09-01 11:29:17 +0530

    solved

--- set.py
+++ set.py
@@ -7,6 +7,24 @@
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
+
+def allSets(n):
+    size = 2 ** n
+    lst = []
+    for i in range(size):
+        s = set()
+        for j in range(size):
+            if i & (1 << j) > 0:
+                s.add(j+1)
+        lst.append(s)
+    return lst
+
 def limitedPowerSet(n, k):
-    # Your code goes here...
-    pass
+    sets = sorted(allSets(n), key = len)
+    res = sets[:k]
+    res[0] = {}
+    return res
+
+
+assert(limitedPowerSet(5,7) == [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ])
+print("Test case passed")
